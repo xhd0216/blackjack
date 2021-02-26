f4d232f59ceb46dbdfc1ea1cc38068ae56e0a072
B. Garage <xhd0216@gmail.com>
2021-02-25 18:54:42 -0800

    get images

--- feiwu/views.py
+++ feiwu/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 from random_thoughts.options_codes.download_prices import extract_data_wrapper
 import json
 
+import plotly.express as px
+
+def get_image():
+    fig =px.scatter(x=range(10), y=range(10))
+    return fig.to_html(include_plotlyjs="cdn")
+
+    
+
 def index(req):
     _, data = extract_data_wrapper("aapl")
-    resp = HttpResponse(json.dumps(data, indent=4, sort_keys=True))
+    #resp = HttpResponse(json.dumps(data, indent=4, sort_keys=True))
+    resp = HttpResponse(get_image())
     return resp
