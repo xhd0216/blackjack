1951fd66afb0c5e2a41a0667ec116c94c028de63
B. Garage <xhd0216@gmail.com>
2021-03-04 16:23:54 -0800

    working solution

--- feiwu/views.py
+++ feiwu/views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
-from randomthoughts.stock_codes.download_prices import stock_download
+from randomthoughts.drawing.tech_lib import get_image
 import json
-
-import plotly.graph_objects as go
-
-
-def get_image(data):
-    fig = go.Figure([go.Scatter(x=data["timestamp"], y=data["close"])])
-    #fig = go.Figure([go.Scatter(x=data["timestamp"], y=data["close"]), go.Bar(x=data["timestamp"], y=data["volume"])])
-    return fig.to_html(include_plotlyjs="cdn")
 
 
 def index(req):
-    data = stock_download("tsla", "1d", "1y")
-    #raise ValueError("here")
-    resp = HttpResponse(get_image(data))
-    #resp = JsonResponse(data)
+    fig = get_image()
+    resp = HttpResponse(fig)
 
     return resp
