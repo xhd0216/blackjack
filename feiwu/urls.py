d831fa7271b51cc2669da225bf7ffa0280f7182b
Joe Lin <xhd0216@gmail.com>
2021-10-25 05:13:36 +0000

    return png in a file

--- feiwu/urls.py
+++ feiwu/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("q", views.get_symbol, name="drawing"),
+    path("figure", views.generate_pic, name="figure"),
 ]
--- feiwu/views.py
+++ feiwu/views.py
@@ -2,8 +2,8 @@ import json
 import os
 
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseServerError
-# Create your views here.
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseServerError, FileResponse
+
 from randomthoughts.drawing.tech_lib import get_image
 from trendlines.server import drawing
 
@@ -14,24 +14,28 @@ html = """
     <title>picture</title>
 </head>
 <body>
-    <img src="/static/%s" alt="haha">
+    <img src="/random/figure" alt="haha">
 </body>
 </html>
 """
+
+def generate_pic(req):
+    """ download data and generate figure """
+    file_path = os.environ['STATICIMGPATH']
+    filename = drawing(None, file_path=file_path)
+    try:
+        resp = FileResponse(open(os.path.join(file_path, filename)))
+    except Exception as e:
+        return HttpResponseServerError("exception")
+    return resp
+
 
 def get_symbol(req):
     """ main api """
     symbol = req.GET.get("s", None)
     gap = req.GET.get("g", None)
     rng = req.GET.get("r", None)
-    try:
-      #fig = get_image(symbol, gap, rng)
-      filename = drawing(None, file_path=os.environ['STATICIMGPATH'])
-    except Exception as e:
-      return HttpResponseServerError("found exception %s" % str(e))
-
-    #resp = HttpResponse(fig)
-    resp = HttpResponse(html % filename)
+    resp = HttpResponse(html)
     return resp
 
 def index(req):
