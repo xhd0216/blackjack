5e174aafa3f8f5fbc97413b270b87e38bb0f8fa5
Joe Lin <xhd0216@gmail.com>
2021-11-20 01:32:26 +0000

    use web api from trendlines

--- feiwu/views.py
+++ feiwu/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseServerError, FileResponse
 
 from randomthoughts.drawing.tech_lib import get_image
-from trendlines.server import drawing
+from trendlines.server import drawing, get_image_full_path
+from trendlines.web import get_tick_id
+
 
 html = """
 <!DOCTYPE html>
@@ -22,10 +24,7 @@ html = """
 def generate_pic(req):
     """ download data and generate figure """
     filename = req.GET.get("id", None)
-    if filename:
-        filename += '.png'
-    file_path = os.environ['STATICIMGPATH']
-    full_file_path = os.path.join(file_path, filename)
+    full_file_path = get_image_full_path(None, filename)
     try:
         resp = FileResponse(open(full_file_path, 'r'))
     except Exception as e:
@@ -33,22 +32,46 @@ def generate_pic(req):
     return resp
 
 
-def get_symbol(req):
-    """ main api """
-    symbol = req.GET.get("s", None)
-    gap = req.GET.get("g", None)
-    rng = req.GET.get("r", None)
+REQ_PARAM = {
+    "s": "symbol",
+    "g": "interval",
+    "r": "range",
+}
+
+def fetch_image(comp_cfg):
     try:
         file_path = os.environ['STATICIMGPATH']
-        filename = drawing(None, file_path=file_path)
+        filename = drawing(None, file_path=file_path, comp_cfg=comp_cfg)
         file_id = filename.split('.')[0]
+        return file_id
+    except Exception as e:
+        raise e
+
+
+def get_image_id(cfg):
+    """ get image id """
+    return next(get_tick_id(cfg), None)
+
+
+def get_symbol(req):
+    """ main api """
+    comp_cfg = {}
+    for k in REQ_PARAM:
+        if k in req.GET and req.GET[k] is not '':
+            comp_cfg[REQ_PARAM[k]] = req.GET[k]
+
+    try:
+        # FIXME: get_image_id should have full config
+        file_id = get_image_id(comp_cfg)
+        if not file_id:
+            file_id = "NOT_READY"
         referer = req.headers.get("X-FROM-TG", None)
         if referer:
             resp = HttpResponse(file_id)
         else:
             resp = HttpResponse(html % file_id)
     except Exception as e:
-        return HttpResponseServerError("exception happened")
+        resp = HttpResponseServerError("exception happened %s" % str(e))
 
     return resp
 
